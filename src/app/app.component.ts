1e21fc6fa105e42deccb19fec86f1e18dce37dac
mohamed-ahmed10 <eng.mohamedahmed56@gmail.com>
2024-11-11 07:26:26 +0200

    Hero section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,10 @@ import { FooterComponent } from "./layout/footer/footer.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, FooterComponent],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  
+
 }
--- src/app/views/not-found/not-found.component.ts
+++ src/app/views/not-found/not-found.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NavComponent } from "../../layout/nav/nav.component";
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NavComponent,NavComponent],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
